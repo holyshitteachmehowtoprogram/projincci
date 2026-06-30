96971e8f221ccb25e710aaa1c264bbffa7bd9cb6
王九儿 <wangjiuer@wangjiuerdeMacBook-Air.local>
2026-06-30 15:08:47 +0100

    finish lab04 self portrait

---
+++ session2/session02_demo04 copy 2/sketch.js
@@ -0,0 +1,41 @@
+let circlesCount = 20;
+let canvasWidth = 400;
+let canvasHeight = 400;
+
+let spacing = canvasHeight / circlesCount;
+let offset = 10;
+
+function setup() {
+    createCanvas(canvasWidth, canvasHeight);
+    noLoop();
+
+    fill('green')
+    noStroke();
+
+    angleMode(DEGREES);
+}
+
+function draw() {
+    background(220);
+
+    for (let i = 0; i < circlesCount; i++) {
+        for(let j = 0; j < circlesCount; j++) {
+            push();
+            translate(i * spacing + offset, j * spacing + offset);
+            let rotaition = random([0, 90, 180, 270]);
+            rotate(rotaition);
+
+            triangle(
+                10, // x1
+                10, // y1
+
+                -10, // x1
+                -10, // y1
+
+                -10, // x1
+                10, // y1
+                );
+                pop();
+        }
+    }
+}
